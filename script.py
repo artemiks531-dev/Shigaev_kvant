50c06d9c597145e6b66ce841318ee7821309f6f2
artem <artemiks531@gmail.com>
2026-06-26 12:06:47 +0300

    квартес

--- script.py
+++ script.py
@@ -1,1 +1,14 @@
-print('hello worgld')
+def privet(who_from, **who_to):
+    who_to_list = []
+
+    items = list(who_to.items())
+    for item in items:
+        who_to_list.append(', '.join(item))
+
+    print(
+        f'вам привет от {who_from}!:\n'
+        f'кому: {'\n'.join(who_to_list)}\n'
+    )
+
+
+privet('artiom', raushan='hello bro', karim='krutoy')
